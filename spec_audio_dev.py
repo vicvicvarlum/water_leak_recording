ec9a40f028cce3363f4ef009a87bda57760a2a22
vicvicvarlum <victor.vargas@lumenir.com>
2021-09-03 12:49:24 -0600

    Added continuous loop
    Will record when triggered and stop when no audio (RMS) is present over a threshold

--- spec_audio_dev.py
+++ spec_audio_dev.py
@@ -1,10 +1,12 @@
 #pip install pipwin
 #pipwin install pyaudio
 
+from typing import Tuple
 import pyaudio
 import wave
 import math
 import struct
+import time
 
 form_1 = pyaudio.paInt16 # 16-bit resolution
 chans = 1 # 1 channel
@@ -13,6 +15,10 @@ chunk = 4096 # 2^12 samples for buffer
 record_secs = 10 # seconds to record
 dev_index = 1 # device index found by p.get_device_info_by_index(ii)
 wav_output_filename = 'test1.wav' # name of .wav file
+
+wav_output_filename2 = 'tt1.wav' # name of .wav file
+
+print(type(wav_output_filename2))
 
 RATE = 44100  
 INPUT_BLOCK_TIME = 0.01
@@ -25,7 +31,7 @@ audio = pyaudio.PyAudio() # create pyaudio instantiation
 stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
                     input_device_index = dev_index,input = True, \
                     frames_per_buffer=INPUT_FRAMES_PER_BLOCK)
-print("recording")
+
 frames = []
 
 def get_rms(block):
@@ -56,6 +62,12 @@ mute_tolerance = 5 * 100 #seconds
 threshold_flag_when_trigger = 0
 append_en_timeout = False
 
+build_new_file_exists = False
+
+filename_audio = ""
+
+print(filename_audio)
+print(type(filename_audio))
 while True:
     
     data = stream.read(INPUT_FRAMES_PER_BLOCK)
@@ -64,11 +76,16 @@ while True:
     #print(rms_val, count)
 
     if(rms_val > THRESHOLD):
-        print("T")
+        print("T", count, rms_val)
         frames.append(data)
         threshold_flag_when_trigger = count
         threshold_flag = True
         append_en_timeout = True
+
+        if(build_new_file_exists == False):
+            filename_audio = time.ctime().replace(" ","").replace(":","_") + "_sample.wav"
+            print("New file started: ", filename_audio)
+            build_new_file_exists = True
     else:
         threshold_flag = False
 
@@ -79,10 +96,21 @@ while True:
 
     if(count > threshold_flag_when_trigger + mute_tolerance):
         append_en_timeout = False
+        # save the audio frames as .wav file
+        print("New file saved: ", filename_audio)
+        wavefile = wave.open(filename_audio,'wb')
+        wavefile.setnchannels(chans)
+        wavefile.setsampwidth(audio.get_sample_size(form_1))
+        wavefile.setframerate(samp_rate)
+        wavefile.writeframes(b''.join(frames))
+        wavefile.close()
+        frames.clear()
+        build_new_file_exists = False
+        count = 0
 
     count +=1
-    if(count >seconds_to_sample):
-        break
+    #if(count >seconds_to_sample):
+        
     
 
 print("finished recording")
@@ -92,11 +120,5 @@ stream.stop_stream()
 stream.close()
 audio.terminate()
 
-# save the audio frames as .wav file
-wavefile = wave.open(wav_output_filename,'wb')
-wavefile.setnchannels(chans)
-wavefile.setsampwidth(audio.get_sample_size(form_1))
-wavefile.setframerate(samp_rate)
-wavefile.writeframes(b''.join(frames))
-wavefile.close()
+
 
